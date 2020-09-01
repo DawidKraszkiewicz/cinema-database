80eb5e012d1cc8e9d4c801f6f9bf5ce7687debd5
softline <kraszka1234@gmail.com>
2020-09-01 10:50:51 +0200

    all crud operations working

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { response } = require('express');
-const db = require('./queries');
 const app = express();
+const db = require('./queries');
+
 const port = 3000;
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: true
     })
-)
+);
 app.get('/', (request, response) => {
     response.json({info: 'working'})
 })
 app.get('/film', db.getFilms);
 app.get('/film/:id', db.getFilmById);
 app.post('/film', db.createFilm);
+app.put('/film/:id', db.updateFilm)
+app.delete('/film/:id', db.deleteFilmById)
 app.listen(port, ()=>{
     console.log(`app running on port ${port} `)
 })
--- queries.js
+++ queries.js
@@ -28,8 +28,11 @@ const getFilmById = (request, response)=>{
         response.status(200).json(results.rows)
     })
 }
-const createFilm = (request,response)=>{
-    const { film_title, year, director_id, genre } = request.body
+const createFilm = (request, response)=>{
+    const film_title = request.body.film_title
+    const genre = request.body.film_genre
+    const year = request.body.year
+    const director_id = request.body.director_id
     pool.query('INSERT INTO film (film_title, year, director_id, genre) VALUES( $1, $2, $3, $4)',[film_title, year, director_id, genre], (error, results)=>{
         if(error){
             throw error;
@@ -37,8 +40,39 @@ const createFilm = (request,response)=>{
         response.status(201).send(`film added with id: ${results.insertId}`)
     })
 }
+const updateFilm= (request, response) => {
+    const id = parseInt(request.params.id)
+    const film_title = request.body.film_title
+    const genre = request.body.film_genre
+    const year = request.body.year
+    const director_id = request.body.director_id
+
+    pool.query(
+        'UPDATE film SET film_title = $1, genre = $2, year = $3, director_id = $4 WHERE id = $5',
+        [film_title, genre, year, director_id, id], 
+        (error, results) => {
+            if(error){
+                throw error;
+            }
+            response.status(200).send(`film modified with id ${id}`)
+        }
+    )
+}
+const deleteFilmById = (request, response) =>{
+    const id = parseInt(request.params.id)
+    pool.query('DELETE FROM film WHERE id = $1', [id], (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).send(`deleted film from db with id: ${id}`)
+    })
+
+}
+
 module.exports = {
     getFilms: getFilms,
     getFilmById: getFilmById,
-    createFilm: createFilm
+    createFilm: createFilm,
+    deleteFilmById: deleteFilmById,
+    updateFilm: updateFilm,
 }
