76826df0611040aa3f705ea96bea41088aa18ec6
softline <kraszka1234@gmail.com>
2020-09-04 13:42:44 +0200

    added CORS

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./Controllers/filmController/filmRouter.js', './Controllers/actorController/actorRouter.js')
-
+const filmRouter = require('./Controllers/filmController/filmRouter')
+const actorRouter = require('./Controllers/actorController/actorRouter')
 const app = express();
 const port = 3000;
+
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: true
     })
 );
-app.use(router)
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(actorRouter, filmRouter)
+
+  
 app.listen(port, ()=>{
     console.log(`app running on port ${port} `)
 })
+
